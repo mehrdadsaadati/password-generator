44d639ce2c65cb93ce090403d9bc8f234c37b469
mehrdadsaadati <63453421+mehrdadsaadati@users.noreply.github.com>
2024-08-26 16:29:22 +0330

    refactor: reduced complexity by removing dto layer and moving argument parse login to the cli script

--- password_generator/cli.py
+++ password_generator/cli.py
@@ -1,6 +1,51 @@
+import argparse
+
+
 def main():
     """Main entry point of the app"""
-    pass
+
+    # parse user input arguments
+    parser = argparse.ArgumentParser(
+        prog="password_generator",
+        description="Generates secure random passwords based on user inputs",
+    )
+    # add positional argument len
+    parser.add_argument("len", type=int)
+    # add option format
+    parser.add_argument(
+        "-f",
+        "--format",
+        default=None,
+        type=str,
+        help="""Format of the password (String). Format of the password. Can be one of these options or a mix of them:
+    'a': lower case characters
+    'A': lower case characters
+    '0': numbers from 0 to 9
+    '@': symbols
+    'h': lower case hex number (other formats will be ignored)
+    'H': upper case hex number (other formats will be ignored)
+""",
+    )
+    # add option exclude
+    parser.add_argument(
+        "-e",
+        "--exclude",
+        default=None,
+        type=str,
+        help="A string of characters (letters, numbers and symbols) to exclude from the generated password",
+    )
+    # add option custom
+    parser.add_argument(
+        "-c",
+        "--custom",
+        default=None,
+        type=str,
+        help="A custom string of letters, numbers and symbols to pick characters from. If this parameter is passed, format is ignored",
+    )
+
+    args = parser.parse_args()
+
+    print(args)
 
 
 if __name__ == "__main__":
